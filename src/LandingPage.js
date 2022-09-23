89f0517ff9f577a88110f2ab8f4af425fb204219
TheWaler <tom@decodify.org>
2022-09-23 15:38:37 +0100

    Initial Commit

---
+++ src/LandingPage.js
@@ -0,0 +1,14 @@
+import dawn from './dawn.png';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <header className="LandingPage-header">
+        <img src={dawn} />
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
